068ea3e399222034806f2ddefe03b9daa1063bee
darkroom51 <darkroom@wp.pl>
2018-04-07 18:07:41 +0200

    added GET /todos/:id

--- 5-node-todo-api/playground/mongoose-queries.js
+++ 5-node-todo-api/playground/mongoose-queries.js
@@ -7,7 +7,7 @@ const { User } = require('./../server/models/user');
 var id = '5ac8cad60fe73f104412a42d';
 var wrongId = '6ac8cad60fe73f104412a42d';
 var invalidId = 'A6ac8cad60fe73f104412a42d';
-var userId = 'z5ac7da389b3b1b251c30f5f9';
+var userId = '5ac7da389b3b1b251c30f5f9';
 
 if (!ObjectID.isValid(invalidId)) { // check ID is valid pattern
 	console.log('Todo Id not valid');
--- 5-node-todo-api/server/server.js
+++ 5-node-todo-api/server/server.js
@@ -1,39 +1,64 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json()); //app.user takes middleware
 
+// POST /todos
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
-        text: req.body.text //data from json from request
-    });
-    todo.save()
-        .then((doc) => {
-            res.send(doc);
-        }, (err) => {
-            res.status(400).send(err);
-        })
+	var todo = new Todo({
+		text: req.body.text //data from json from request
+	});
+	todo.save()
+		.then((doc) => {
+			res.send(doc);
+		}, (err) => {
+			res.status(400).send(err);
+		})
 });
 
+// GET /todos
 app.get('/todos', (req, res) => {
-    Todo.find()
-        .then((todos) => {
-            res.send({todos})
-        }, (e) => {
-            res.status(400).send(e);
-        })
+	Todo.find()
+		.then((todos) => {
+			res.send({ todos }) // {todos: todos}
+		}, (e) => {
+			res.status(400).send(e);
+		})
 });
 
+// GET /todos/1243242432
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) { // invalid id pattern
+		console.log('Invalid Id pattern');
+		return res.status(404).send(); // 404 and empty response
+	}
+
+	Todo.findById(id)
+		.then((todo) => {
+			if (!todo) { // id OK pattern but not found so todo === null
+				return res.status(404).send({});
+			}
+			res.send({ todo }); // {todo} ---> {todo: todo}
+		}, (e) => { // promise rejected
+			res.status(400).send();
+			console.log('Promise rejected:', e);
+		});
+});
+
+// server listen
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log('Server started on port 3000');
+	console.log('Server started on port 3000');
 });
 
 
-module.exports = {app};
+module.exports = { app };
